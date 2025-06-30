9bc2d98748a9028cfee1c58a6c6ed29999a67e36
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-30 18:36:21 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './style/global.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router'
+import './style/global.css'
+import './style/tailwind.css'
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app')
