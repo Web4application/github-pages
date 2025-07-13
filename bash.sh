548ba2fc91d080e9e0b977204017223e3299f75c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-13 20:55:13 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,9 @@
-npm install vue-router@4 tailwindcss@3 postcss autoprefixer -D
-npx tailwindcss init -p
+$ npm install
+$ npm run dev
 
-npm run serve
+$ npm install vue-router@4 tailwindcss@3 postcss autoprefixer -D
+$ npx tailwindcss init -p
 
-npm run build
+$ npm run serve
+
+$ npm run build
