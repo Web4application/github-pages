08b57b71fe04b37fd5a1b8dd7b91915d834414db
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-27 04:30:03 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,15 @@
-$ npm install
-$ npm run dev
+ npm install
+ npm run dev
 
-$ npm install vue-router@4 tailwindcss@3 postcss autoprefixer -D
-$ npx tailwindcss init -p
+npm install vue-router@4 tailwindcss@3 postcss autoprefixer -D
+ npx tailwindcss init -p
 
-$ npm run serve
+npm run serve
 
-$ npm run build
+ npm run build
+
+npm run build
+git checkout gh-pages
+cp -r dist/* .
+git commit -am "Deploy"
+git push origin gh-pages
