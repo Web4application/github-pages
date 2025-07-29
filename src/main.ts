12a63b3b8026ae8dc02854a33239be972f479bb6
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-29 22:44:53 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+// src/main.ts
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
-import './style/global.css'
-import './style/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
