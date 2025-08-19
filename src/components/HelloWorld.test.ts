3119c4ada81ad54dca1deb140d518aaf83cbf768
Web4 <167559384+Web4application@users.noreply.github.com>
2025-08-19 02:32:15 +0100

    Delete src/components/HelloWorld.test.ts

--- src/components/HelloWorld.test.ts
+++
@@ -1,7 +0,0 @@
-import { mount } from "@vue/test-utils";
-import HelloWorld from "./HelloWorld.vue";
-
-test("renders props.msg", () => {
-  const wrapper = mount(HelloWorld, { props: { msg: "Hello Vue" } });
-  expect(wrapper.text()).toContain("Hello Vue");
-});
